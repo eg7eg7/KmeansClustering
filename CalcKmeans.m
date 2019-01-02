8a337a5d95102ca09656581e1c56466930e40bd7
Eden Dupont <eg7eg7@gmail.com>
2019-01-03 00:38:31 +0200

    comments

--- CalcKmeans.m
+++ CalcKmeans.m
@@ -41,6 +41,27 @@ for i=1:K
     Clusters(:,i)=column;
 end
 
+%% 2-Calculate distance for each datapoint to each cluster and assign to
+%% closest cluster center
+
+%% 3- Calculate error - defined to be the average distance of a node to it's
+%% parent
+
+%% 4 - Calculate average for each dimension using data points in clusters and
+%% refresh K co-ordinates
+
+%% repeat 2 and 3 until halting criteria is met:
+% a. no cluster is changed
+%
+% b. max iterations reached
+%
+% c. changes in cluster centers are minor
+%
+% d. The change in error rate is under a certain percentage OR the error
+% rate is under the minimum value
+
+
+
 
 %stub - delete later
 Err=0;
