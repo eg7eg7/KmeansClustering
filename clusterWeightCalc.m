e5bcb6bf769b252dbd5b5628988ec4c777096a25
Daniel Rolnik <danrol92@gmail.com>
2019-01-04 11:50:46 +0200

    weight calc

--- clusterWeightCalc.m
+++ clusterWeightCalc.m
@@ -1,6 +1,6 @@
-function [Clusters, cluster_changes] = clusterWeightCalc(INDX,DataMATRIX)
+function [Clusters, cluster_changes] = clusterWeightCalc(INDX,DataMATRIX, prev_cluster_changes)
 
-%   Clusters is weight matrix - n rows (dimension) and K columns (number of clusters) 
+%   Clusters is weight matrix - n rows (dimension) and K columns (number of clusters)
 %   cluster_changes is the percentage of change the weights moved from
 %   their previous location
 %
@@ -8,3 +8,20 @@ function [Clusters, cluster_changes] = clusterWeightCalc(INDX,DataMATRIX)
 %   cluster
 %
 %   DataMATRIX is the data, rows(dimension), columns (num of data points)
+
+
+[n,m] = size(INDX);
+Clusters(1:m,1:K) = 0;
+num_points_in_cluster(1:1,1:K) = 0;
+
+for i = 1:m
+    cluster = INDX(i);
+    num_points_in_cluster(1,cluster) = num_points_in_cluster(1,cluster) + 1; 
+    Clusters(:,cluster) = Clusters(:,cluster) + DataMATRIX(:,i);
+end
+
+for i=1:m
+    Clusters(:,i) = Clusters(:,i)/num_points_in_cluster(1,i);
+    
+end
+
