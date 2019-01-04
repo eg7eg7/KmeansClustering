92d9394f9599389916189d53420308353f7901e2
Daniel Rolnik <danrol92@gmail.com>
2019-01-04 22:10:45 +0200

    clusterWeightCalc. Need to decide how to use cluster_changes

--- clusterWeightCalc.m
+++ clusterWeightCalc.m
@@ -10,9 +10,9 @@ function [Clusters, cluster_changes] = clusterWeightCalc(INDX,DataMATRIX, prev_c
 %   DataMATRIX is the data, rows(dimension), columns (num of data points)
 
 
-[n,m] = size(INDX);
-Clusters(1:size(DataMATRIX,1),1:K) = 0;
-cluster_changes(1:1, 1:K) = 0;
+[n,m] = size(DataMATRIX);
+Clusters(1:n,1:K) = 0;
+% cluster_changes(1:1, 1:K) = 0;
 num_points_in_cluster(1:1,1:K) = 0;
 
 for i = 1:m
@@ -22,19 +22,27 @@ for i = 1:m
 end
 
 % TODO need to be fixed
-for i=1:size(DataMATRIX,1)
+
+for i=1:n
     Clusters(:,i) = Clusters(:,i)/num_points_in_cluster(1,i);
-    cluster_row(1:size(DataMATRIX,1), 1:1) = 0;
-    for k=1:size(DataMATRIX,1)
-        %there is problem in this inner loop. prev_cluster_cell_value and
-        %new_cluster_cell_value get relevant cell value only through first
-        %iteration
-        prev_cluster_cell_value = prev_clusters(k:i);
-        new_cluster_cell_value = Clusters(k:i);
-        cluster_change = abs(prev_cluster_cell_value/new_cluster_cell_value);
-        cluster_row(k) = cluster_change;
-    end
-    cluster_row = cluster_row*100;
-    %perform average
-    cluster_changes(1, i) = mean(cluster_row(k,:));
+    
+%     cluster_row(1:size(DataMATRIX,1), 1:1) = 0;
+    
+%     for j=1:m
+%         %there is problem in this inner loop. prev_cluster_cell_value and
+%         %new_cluster_cell_value get relevant cell value only through first
+%         %iteration
+%         prev_cluster_cell_value = prev_clusters(j:i);
+%         new_cluster_cell_value = Clusters(j:i);
+%         cluster_change = abs(prev_cluster_cell_value/new_cluster_cell_value);
+%         cluster_row(j) = cluster_change;
+%     end
+%     cluster_row = cluster_row*100;
+%     %perform average
+%     cluster_changes(1, i) = mean(cluster_row(j,:));
 end
+cluster_changes_matrix = abs(Clusters-prev_clusters);
+% cluster_changes_matrix = cluster_changes_matrix *100;
+cluster_changes = sum(sum(cluster_changes_matrix));
+cluster_changes = cluster_changes / (n*K);
+disp('print');
